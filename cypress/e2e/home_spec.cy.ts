8b7b9a5106416ea2a23bf80256cc382e9e560475
MohammedAbdelraoof <Mohammed.abdelraoof@syngenta.com>
2023-03-25 04:34:56 +0200

    remove unused code

--- cypress/e2e/home_spec.cy.ts
+++ cypress/e2e/home_spec.cy.ts
@@ -1,43 +1,36 @@
-describe('The Home Page', () => {
-  beforeEach(() => {
-    cy.visit('/')
-    cy.intercept(
-      'https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json',
-      { fixture: 'posts' }
-    ).as('getPosts')
-  })
+beforeEach(() => {
+  cy.visit("/")
+  cy.intercept(
+    "https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json",
+    { fixture: "posts" }
+  ).as("getPosts")
+})
 
-  it('card is rendered', () => {
+describe("The Home Page", () => {
+  it("card is rendered", () => {
     cy.get('[data-cy="blog_card"]')
   })
 
-  it('posts data successfully loads', () => {
+  it("posts data successfully loads", () => {
     cy.get('[data-cy="title"]')
       .first()
       .should(
-        'have.text',
-        'Travel, CLIs, and sticky notes: Lilyana’s life as a Canonical UX designer'
+        "have.text",
+        "Travel, CLIs, and sticky notes: Lilyana’s life as a Canonical UX designer"
       )
     cy.get('[data-cy="header"]')
       .first()
-      .should('have.text', 'People and culture')
+      .should("have.text", "People and culture")
   })
 })
 
-describe('Posts API response & data validation', () => {
-  beforeEach(() => {
-    cy.visit('/')
-    cy.intercept(
-      'https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json',
-      { fixture: 'posts' }
-    ).as('getPosts')
-  })
-  it('posts api response & not empty data', () => {
-    cy.wait('@getPosts').its('response.statusCode').should('eq', 200)
+describe("Posts API response & data validation", () => {
+  it("posts api response & not empty data", () => {
+    cy.wait("@getPosts").its("response.statusCode").should("eq", 200)
   })
 
-  it('non empty data api Data', () => {
-    cy.wait('@getPosts').its('response.body').should('not.have.length', 0)
-    cy.wait('@getPosts').its('response.body').should('have.length', 2)
+  it("non empty data api Data", () => {
+    cy.wait("@getPosts").its("response.body").should("not.have.length", 0)
+    cy.wait("@getPosts").its("response.body").should("have.length", 2)
   })
 })
